97b910b2bf1f24b831683014bbf89e886947bdba
prepodobnuy <zvfvbycsy6@gmail.com>
2024-06-24 01:21:24 +0300

    fixing shitcode

--- hypr/wallpaper.py
+++ hypr/wallpaper.py
@@ -1,5 +1,6 @@
 # requirements:
 # pywal
+# colorz
 # for xorg wallpaper change
 # feh 
 # for wayland wallpaper change
@@ -30,22 +31,119 @@ HELPMESSAGE = """-h --help                      display this message
 --dconf [path]                 set display config path
 --swww "-param --param 123"    set swww params"""
 
-class display(object):
+DEBUG = False
+
+class Display(object):
+    """
+    Class for containing display params wich is readed from displays file
+    """
     def __init__(self, name:str, width:int, height:int, margin_x:int=0, margin_y:int=0) -> None:
         self.name:str = name
         self.w:int = width
         self.h:int = height
         self.x:int = margin_x
-        self.y:int = margin_y
-        
+        self.y:int = margin_y    
         self.image = None
+
+    def max_width(displays:list) -> int:
+        """Function wich is searching largest width+marginx value from list of displays"""
+        res = 0
+        for display in displays:
+            res = display.w + display.x if display.w + display.x > res else res
+        
+        return res
+
+    def max_height(displays:list) -> int:
+        """Function wich is searching largest height+marginy value from list of displays"""
+        res = 0
+        for display in displays:
+            res = display.h + display.y if display.h + display.y > res else res
+        
+        return res
     
     def __str__(self) -> str:
         return f"{self.name} {self.w}x{self.h} {self.x}x{self.y}"
+    
     def __repr__(self) -> str:
         return f"{self.name} {self.w}x{self.h} {self.x}x{self.y}"
 
-class template(object):
+def read_display_config() -> list[Display]:
+    res:list[Display] = []
+    with open(f"{WORKINGDIR}/displays") as file:
+        data = (file.read()).split('\n')
+
+    for row in data:
+        if row == '': continue
+        if row[0] == '#': continue
+        params = row.split('=')[1].split(',')
+        tmp = Display(
+            name=params[0],
+            width    = int(params[1].split('x')[0]),
+            height   = int(params[1].split('x')[1]),
+            margin_x = int(params[2].split('x')[0]),
+            margin_y = int(params[2].split('x')[1])
+        )
+        res.append(tmp)
+
+    return res
+
+def resize_displays(displays:list[Display], wallpaper_path:str):
+    if DEBUG: print("//resizing displays")
+    maxw = Display.max_width(displays)
+    maxh = Display.max_height(displays)
+
+    image = Image.open(wallpaper_path)
+    imagew, imageh = image.size
+
+    width_dif = imagew / maxw
+    height_dif = imageh / maxh
+
+    for display in displays:
+        display.w = int(display.w * width_dif)
+        display.h = int(display.h * width_dif)
+        display.x = int(display.x * width_dif)
+        display.y = int(display.y * width_dif)
+
+    if DEBUG: print(f"default params:\n\timage {imagew}x{imageh}\n\tdispays {maxw}x{maxh}\n\tdifference {width_dif}x{height_dif}")
+
+    maxw = Display.max_width(displays)
+    maxh = Display.max_height(displays)
+    width_dif = imagew / maxw
+    height_dif = imageh / maxh
+    if DEBUG: print(f"after width scaling:\n\timage {imagew}x{imageh}\n\tdispays {maxw}x{maxh}\n\tdifference {width_dif}x{height_dif}")
+
+    if height_dif < 1:
+        for display in displays:
+            display.w = int(display.w * height_dif)
+            display.h = int(display.h * height_dif)
+            display.x = int(display.x * height_dif)
+            display.y = int(display.y * height_dif)
+
+    maxw = Display.max_width(displays)
+    maxh = Display.max_height(displays)
+    width_dif = imagew / maxw
+    height_dif = imageh / maxh
+    if DEBUG: print(f"after height scaling:\n\timage {imagew}x{imageh}\n\tdispays {maxw}x{maxh}\n\tdifference {width_dif}x{height_dif}")
+
+    return displays
+
+def split_wallpaper(displays, wallpaper):
+    if DEBUG: print("//spliting image")
+    image = Image.open(wallpaper)
+
+    for display in displays:
+        display.image = image.crop((
+            display.x,
+            display.y,
+            display.w + display.x,
+            display.h + display.y 
+        ))
+        if DEBUG: print(f"image: {display.x}x{display.y} {display.w + display.x}x{display.h + display.y}")
+    
+    return displays
+
+class Template(object):
+    """Class for containing config params wich is readed from configs file"""
     def __init__(self, templatepath, configpath, usequotes=False, usesharps=False, command=None, opacity='') -> None:
         self.templatefilepath = templatepath
         self.configfilepath = configpath
@@ -53,12 +151,6 @@ class template(object):
         self.usequotes = usequotes
         self.usesharps = usesharps
         self.opacity = opacity
-
-    def __str__(self) -> str:
-        return f"Command: {self.executeAfter} {self.templatefilepath} -> {self.configfilepath};"
-    
-    def __repr__(self) -> str:
-        return f"Command: {self.executeAfter} {self.templatefilepath} -> {self.configfilepath};"
 
     def apply_colors(self, colors) -> None:
         with open(self.templatefilepath, 'r') as template:
@@ -82,96 +174,47 @@ class template(object):
         if self.executeAfter != None:
             os.popen(self.executeAfter)
 
+    def __str__(self) -> str:
+        return f"Command: {self.executeAfter} {self.templatefilepath} -> {self.configfilepath};"
+    
+    def __repr__(self) -> str:
+        return f"Command: {self.executeAfter} {self.templatefilepath} -> {self.configfilepath};"
+
+def read_templates():
+    res = []
+    with open(f"{WORKINGDIR}/configs") as file:
+        data = (file.read()).split('\n')
+    
+    for row in data:
+        if row == '': continue
+        if row[0] == '#': continue
+        tmpdata = row.split(';')
+        try:
+            commanddata = ';'.join(tmpdata[5::])
+            temp = Template(
+                templatepath=tmpdata[0],
+                configpath=tmpdata[1],
+                usequotes='1' in tmpdata[2],
+                usesharps='1' in tmpdata[3],
+                opacity=tmpdata[4] if not '0' in tmpdata[4] else '',
+                command=commanddata
+            )
+        except IndexError:
+            temp = Template(
+                templatepath=tmpdata[0],
+                configpath=tmpdata[1],
+                usequotes='1' in tmpdata[2],
+                usesharps='1' in tmpdata[3],
+                opacity=tmpdata[4] if not '0' in tmpdata[4] else ''
+            )
+
+        res.append(temp)
+
+    return res
+
 def runrofi(wallpapers):
     result = subprocess.run(['rofi', '-dmenu', '-p'], input='\n'.join(wallpapers).encode(), stdout=subprocess.PIPE)
     return result.stdout.decode()
-
-def max_width(displays:list[display]):
-    res = 0
-    for display in displays:
-        res = display.w + display.x if display.w + display.x > res else res
-    
-    return res
-
-def max_height(displays:list[display]):
-    res = 0
-    for display in displays:
-        res = display.h + display.y if display.h + display.y > res else res
-    
-    return res
-
-def read_display_config() -> list[display]:
-    res:list[display] = []
-    with open(f"{WORKINGDIR}/displays") as file:
-        data = (file.read()).split('\n')
-
-    for row in data:
-        params = row.split('=')[1].split(',')
-        tmp = display(
-            name=params[0],
-            width=int(params[1].split('x')[0]),
-            height=int(params[1].split('x')[1]),
-            margin_x=int(params[2].split('x')[0]),
-            margin_y=int(params[2].split('x')[1])
-        )
-        res.append(tmp)
-
-    return res
-
-def resize_displays(displays:list[display], wallpaper_path:str):
-    #print("//resizing displays")
-    maxw = max_width(displays)
-    maxh = max_height(displays)
-
-    image = Image.open(wallpaper_path)
-    imagew, imageh = image.size
-
-    width_dif = imagew / maxw
-    height_dif = imageh / maxh
-
-    for display in displays:
-        display.w = int(display.w * width_dif)
-        display.h = int(display.h * width_dif)
-        display.x = int(display.x * width_dif)
-        display.y = int(display.y * width_dif)
-
-    #print(f"default params:\n\timage {imagew}x{imageh}\n\tdispays {maxw}x{maxh}\n\tdifference {width_dif}x{height_dif}")
-
-    maxw = max_width(displays)
-    maxh = max_height(displays)
-    width_dif = imagew / maxw
-    height_dif = imageh / maxh
-    #print(f"after width scaling:\n\timage {imagew}x{imageh}\n\tdispays {maxw}x{maxh}\n\tdifference {width_dif}x{height_dif}")
-
-    if height_dif < 1:
-        for display in displays:
-            display.w = int(display.w * height_dif)
-            display.h = int(display.h * height_dif)
-            display.x = int(display.x * height_dif)
-            display.y = int(display.y * height_dif)
-
-    maxw = max_width(displays)
-    maxh = max_height(displays)
-    width_dif = imagew / maxw
-    height_dif = imageh / maxh
-    #print(f"after height scaling:\n\timage {imagew}x{imageh}\n\tdispays {maxw}x{maxh}\n\tdifference {width_dif}x{height_dif}")
-
-    return displays
-
-def split_wallpaper(displays, wallpaper):
-    #print("//spliting image")
-    image = Image.open(wallpaper)
-
-    for display in displays:
-        display.image = image.crop((
-            display.x,
-            display.y,
-            display.w + display.x,
-            display.h + display.y 
-        ))
-        #print(f"image: {display.x}x{display.y} {display.w + display.x}x{display.h + display.y}")
-    
-    return displays
 
 def cacheWallpapers():
     for wallpaperName in os.listdir(WALLPAPERSDIR):
@@ -187,37 +230,7 @@ def cacheWallpapers():
 
     print()
 
-def read_templates():
-    res = []
-    with open(f"{WORKINGDIR}/configs") as file:
-        data = (file.read()).split('\n')
-    
-    for row in data:
-        tmpdata = row.split(';')
-        try:
-            commanddata = ';'.join(tmpdata[5::])
-            temp = template(
-                templatepath=tmpdata[0],
-                configpath=tmpdata[1],
-                usequotes=True if '1' in tmpdata[2] else False,
-                usesharps=True if '1' in tmpdata[3] else False,
-                opacity=tmpdata[4] if not '0' in tmpdata[4] else '',
-                command=commanddata
-            )
-        except Exception:
-            temp = template(
-                templatepath=tmpdata[0],
-                configpath=tmpdata[1],
-                usequotes=True if '1' in tmpdata[2] else False,
-                usesharps=True if '1' in tmpdata[3] else False,
-                opacity=tmpdata[4] if not '0' in tmpdata[4] else ''
-            )
-
-        res.append(temp)
-
-    return res
-
-def main(wallpaperName):
+def main(wallpaperName) -> None:
     wallpaper = f"{WALLPAPERSDIR}/{wallpaperName}" 
     
     os.system(f"python -m pywal -i {wallpaper} -n --backend {WALBACKEND}")
@@ -244,26 +257,33 @@ def main(wallpaperName):
         template.apply_colors(colors)
 
 if __name__ == "__main__":
-    args = sys.argv
+    def in_args(params:list[str]) -> bool:
+        for param in params:
+            if param in sys.argv: return True
+        return False
+    
+    def args_index(params:list[str]) -> int:
+        for param in params:
+            if param in sys.argv: return sys.argv.index(param)+1
 
-    if '-h' in args or '--help' in args:
+    if in_args(['-h', '--help']):
         print(HELPMESSAGE)
         exit()
 
-    if '-p' in args: WALLPAPERSDIR = args[args.index('-p')+1]
-    if '-w' in args: WALBACKEND = args[args.index('-w')+1]
-    if '-s' in args: SLEEPTIME = int(args[args.index('-s')+1])
-    if '--dconf' in args: WORKINGDIR = args[args.index('--dconf')+1]
-    if '--swww' in args: SWWWPARAMS = args[args.index('--swww')+1]
-    if '-r' in args: 
-        tmp = runrofi(os.listdir(WALLPAPERSDIR))
-        main(tmp)         
+    if in_args(['-p']):     WALLPAPERSDIR =     sys.argv[args_index(['-p'])]
+    if in_args(['-w']):     WALBACKEND    =     sys.argv[args_index(['-w'])]
+    if in_args(['-s']):     SLEEPTIME     = int(sys.argv[args_index(['-s'])])
+    if in_args(['--conf']): WORKINGDIR    =     sys.argv[args_index(['--conf'])]
+    if in_args(['--swww']): SWWWPARAMS    =     sys.argv[args_index(['--swww'])]
+    
+    if in_args(['-r']): tmp = runrofi(os.listdir(WALLPAPERSDIR)); main(tmp)         
+    
     else:
         try:
             wallpapers = os.listdir(WALLPAPERSDIR)
             cacheWallpapers()
             while True:
-                tmp, wallpaper = "lol", "lol"
+                tmp, wallpaper = "govnokod", "govnokod"
 
                 while wallpaper == tmp:
                     wallpaperName = random.choice(wallpapers)
@@ -272,7 +292,7 @@ if __name__ == "__main__":
 
                 main(wallpaperName)
 
-                if '--once' in args:
+                if in_args(['--once']):
                     time.sleep(2)
                     exit()
 
