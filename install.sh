e89a1f69582c1d8c29725bb35c1575c19f65bce3
prepodobnuy <zvfvbycsy6@gmail.com>
2024-07-18 12:36:14 +0300

    lalala

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+paru -S python imv python-pip otf-font-awesome swww feh pywal alacritty nemo rofi-lbonn-wayland-git waybar hyprshotgun ttf-jetbrains-mono-nerd ttf-terminus-nerd
+pip install colorz pillow --break-system-packages
+
+mkdir ~/.config/alacritty
+mkdir ~/.config/hypr
+mkdir ~/.config/rofi
+mkdir ~/.config/waybar
+mkdir ~/Documents
+mkdir ~/Documents/Wallpapers
+mkdir ~/Documents/CachedWallpapers
+
+cp -r /.config/alacritty ~/.config/alacritty
+cp -r /.config/hypr ~/.config/hypr
+cp -r /.config/rofi ~/.config/rofi
+cp -r /.config/waybar ~/.config/waybar
